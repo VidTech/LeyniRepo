7040887ba38216e7911c35f7bc07d2731384be60
eythort09 <eythort09@ru.is>
2014-10-30 13:03:10 +0000

    Added working test for board length

---
+++ src/main/java/is/ru/tictactoe/Game.java
@@ -0,0 +1,17 @@
+package is.ru.tictactoe;
+
+public class Game{
+	Board b;
+	Player p1;
+	Player p2;
+
+	public Player checkIfWinner(Board b){
+		return null;
+	}
+
+	public boolean checkIfTie(Board b){
+		return true;
+	}
+
+}
+
--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -9,14 +9,25 @@ public class TicTacToeTest {
 
 	//final TicTacToe test = new TicTacToe();
 
+	public static void printArray(double theBoard[][]) {
+    	for (int row = 0; row < theBoard.length; row++) {
+        	for (int column = 0; column < theBoard[row].length; column++) {
+            	System.out.print(theBoard[row][column] + " ");
+        	}
+        	System.out.println();
+    	}
+	}
+
+	printArray(theBoard[][]);
+
 	@Test
 	public void testBoardInitialize(){
 
 		Board test = new Board();
 
-		assertEquals(3, test.theBoard[0][].length);
-		assertEquals(3, test.theBoard[1][].length);
-		assertEquals(3, test.theBoard[2][].length);
+		assertEquals(3, test.theBoard[0].length);
+		assertEquals(3, test.theBoard[1].length);
+		assertEquals(3, test.theBoard[2].length);
 	}
 
 
