c03ac67b7dde36f68aff47f5227664c9a5db9ed3
eythort09 <eythort09@ru.is>
2014-10-30 12:28:43 +0000

    Need to add this to be able to test Asta's classes

--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -10,10 +10,13 @@ public class TicTacToeTest {
 	//final TicTacToe test = new TicTacToe();
 
 	@Test
-	public void testConstructorGrid(){
+	public void testBoardInitialize(){
 
-		TicTacToe test = new TicTacToe();
-		assertEquals(8, test.grid.length);
+		Board test = new Board();
+
+		assertEquals(3, test.theBoard[0][].length);
+		assertEquals(3, test.theBoard[1][].length);
+		assertEquals(3, test.theBoard[2][].length);
 	}
 
 
