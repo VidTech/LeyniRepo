7fe0d84d688788af8a582062d5cb9f43249bcb7c
astahardar <astash90@gmail.com>
2014-10-31 23:22:52 +0000

    html, javascript, ajax

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -1,8 +1,8 @@
 package is.ru.tictactoe;
 import java.util.Scanner;
 
-//import static spark.Spark.*;
-//import spark.*;
+import static spark.Spark.*;
+import spark.*;
 
 
 public class TicTacToe{
@@ -10,33 +10,39 @@ public class TicTacToe{
 	public static void main(String[] args) {
 
 
-		//staticFileLocation("/public");
-        //setPort(Integer.valueOf(System.getenv("PORT")));
-        //final Game tictactoe = new Game();
+		staticFileLocation("/public");
+        setPort(Integer.valueOf(System.getenv("PORT")));
+        final Game tictactoe = new Game();
 
-		Scanner scanner = new Scanner(System.in);
-		Game theGame = new Game();
+		//Scanner scanner = new Scanner(System.in);
+		//Game theGame = new Game();
+         post(new Route("/add") 
+        {
+            @Override
+       		public Object handle(Request request, Response response)
+            {
 
+            	String pos = request.queryParams("id");
+            	int row = Integer.valueOf(pos.split('_')[0]);
+            	int col = Integer.valueOf(pos.split('_')[0]);
 
+				if(theGame.checkIfWinner()){
+					theGame = new Game();
+				}
+				if(theGame.checkIfTie()){
+					theGame = new Game();
+				}
+				else{
+					char sym = theGame.current.getSymbol();
+					theGame.makeMove(row, col);
+					return sym;
+					
+				}
 
-		while (true) {
-			if(theGame.checkIfWinner()){
-				theGame = new Game();
 			}
-			if(theGame.checkIfTie()){
-				theGame = new Game();
-			}
-			else{
-				int row = scanner.nextInt();
-				int col = scanner.nextInt();
 
-				theGame.makeMove(row, col);
-				theGame.b.displayBoard();
-			}
 
-		}
-
+		});
 
 	}
-
 }
