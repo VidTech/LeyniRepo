e62fb502433fbdd18261e848279591a1d1e161d7
astahardar <astash90@gmail.com>
2014-10-31 14:37:21 +0000

    Added fix for overriding in array

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -1,20 +1,28 @@
 package is.ru.tictactoe;
 import java.util.Scanner;
 
+import static spark.Spark.*;
+import spark.*
+
 
 public class TicTacToe{
 
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
-		Game theGame = new Game();
+
+		staticFileLocation("/public");
+        setPort(Integer.valueOf(System.getenv("PORT")));
+        final Game tictactoe = new Game();
+
+		//Scanner scanner = new Scanner(System.in);
+		//Game theGame = new Game();
 
 		while (true) {
 			if(theGame.checkIfWinner()){
-				theGame.b = new Board();
+				theGame = new Game();
 			}
 			if(theGame.checkIfTie()){
-				theGame.b = new Board();
+				theGame.b = new Game();
 			}
 			else{
 				int row = scanner.nextInt();
