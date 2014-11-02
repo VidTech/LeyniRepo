8c2a255c58b0bac031140472822ff729467bd1bb
eythort09 <eythort09@ru.is>
2014-11-02 00:19:01 +0000

    Adding first selenium test, trying to get it to work. Not working atm

---
+++ src/selenium/java/is/ru/tictactoe/TicTacToeWeb.java
@@ -0,0 +1,67 @@
+package is.ru.tictactoe;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.WebDriver;
+
+
+public class TicTacToeWeb {
+
+    static WebDriver driver;
+    static String baseUrl;
+    static String port;
+
+    @BeforeClass
+    public static void before() {
+        // Will be run before all tests in class are run
+        driver = new FirefoxDriver();
+        port = System.getenv("PORT");
+        if(port == null) {
+            port = "4567";
+        }
+        baseUrl = "http://localhost:" + port;
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public static void after() {
+        // Will be run after all tests in class have run
+        driver.close();
+    }
+
+    @Before
+    public void setup() {
+        // Will be run before each test in class is run
+        ;
+    }
+
+    @Test
+    public void assertTitle() {
+        driver.get(baseUrl + "/");
+        assertEquals("TicTacToe", driver.getTitle());
+    }
+}
+
+//     @Test
+//     public void assertSpecificJoke() {
+//         driver.get(baseUrl + "/specific.html");
+//         driver.findElement(By.id("number")).clear();
+//         driver.findElement(By.id("number")).sendKeys("5");
+//         driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+//         WebDriverWait wait = new WebDriverWait(driver, 10);
+//         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert")));
+//         String expectedJoke = "Chuck Norris lost his virginity before his dad did.";
+//         String foundJoke = driver.findElement(By.id("results")).getText();
+//         assertEquals(foundJoke, expectedJoke);
+//     }
+// }
